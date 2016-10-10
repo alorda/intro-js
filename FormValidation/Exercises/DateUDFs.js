575769db70dc0b162c5f638f081ae8d186caf1d4
alorda <alordiux@gmail.com>
2016-10-10 15:18:19 -0500

    Txt field to txt field challenge done, differentlythan solutions, and I think, more elegantly.

--- FormValidation/Exercises/DateUDFs.js
+++ FormValidation/Exercises/DateUDFs.js
@@ -1,6 +1,12 @@
 function monthAsString(num){
-	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	return months[num-1];
+
+	if (num < 1 || num > 12 || isNaN(num) ) {
+		alert("Bad Number!");
+		return "Bad Number!";
+	} else {
+		var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		return months[parseInt(num)-1];
+	}
 }
 
 function dayAsString(num){
